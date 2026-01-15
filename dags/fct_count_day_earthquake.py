76d540ad8705a4a1d551fd7e6025cdab4b95d4b5
V.Goren <holymind73@gmail.com>
2026-01-15 19:52:11 +0300

    Рефактор дагов

--- dags/fct_count_day_earthquake.py
+++ dags/fct_count_day_earthquake.py
@@ -1,131 +1,86 @@
 import pendulum
-from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow                                    import DAG
+from airflow.operators.empty                    import EmptyOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.sensors.external_task              import ExternalTaskSensor
 
-# Конфигурация DAG
-OWNER = "i.korsakov"
-DAG_ID = "fct_count_day_earthquake"
+# переменные из метаданных AIRFLOW
+if True:
+    # Конфигурация DAG
+    OWNER             = "V.Gorenkov"
+    DAG_ID            = "fct_count_day_earthquake"
 
-# Используемые таблицы в DAG
-LAYER = "raw"
-SOURCE = "earthquake"
-SCHEMA = "dm"
-TARGET_TABLE = "fct_count_day_earthquake"
+    LONG_DESCRIPTION  = """
+    # LONG DESCRIPTION
+    """
 
-# DWH
-PG_CONNECT = "postgres_dwh"
+    SHORT_DESCRIPTION = "SHORT DESCRIPTION"
 
-LONG_DESCRIPTION = """
-# LONG DESCRIPTION
-"""
-
-SHORT_DESCRIPTION = "SHORT DESCRIPTION"
-
-args = {
-    "owner": OWNER,
-    "start_date": pendulum.datetime(2025, 5, 1, tz="Europe/Moscow"),
-    "catchup": True,
-    "retries": 3,
-    "retry_delay": pendulum.duration(hours=1),
+    args = {
+    "owner"      : OWNER,
+    "start_date" : pendulum.datetime(2025, 5 , 1, tz = "Europe/Moscow"),
+    "catchup"    : True,
+    "retries"    : 3,
+    "retry_delay": pendulum.duration(hours = 1),
 }
+
+# Другие переменные
+if True:
+    # Используемые таблицы в DAG
+    LAYER             = "raw"
+    SOURCE            = "earthquake"
+    SCHEMA            = "dm"
+    TARGET_TABLE      = "fct_count_day_earthquake"
+
+    # DWH
+    PG_CONNECT        = "postgres_dwh"
 
 
 with DAG(
-    dag_id=DAG_ID,
-    schedule_interval="0 5 * * *",
-    default_args=args,
-    tags=["dm", "pg"],
-    description=SHORT_DESCRIPTION,
-    concurrency=1,
-    max_active_tasks=1,
-    max_active_runs=1,
+    dag_id            = DAG_ID,
+    schedule_interval = "0 5 * * *",
+    default_args      = args,
+    tags              = ["dm", "pg"],
+    description       = SHORT_DESCRIPTION,
+    concurrency       = 1,
+    max_active_tasks  = 1,
+    max_active_runs   = 1,
 ) as dag:
     dag.doc_md = LONG_DESCRIPTION
 
-    start = EmptyOperator(
-        task_id="start",
-    )
-
-    sensor_on_raw_layer = ExternalTaskSensor(
-        task_id="sensor_on_raw_layer",
-        external_dag_id="raw_from_s3_to_pg",
-        allowed_states=["success"],
-        mode="reschedule",
-        timeout=360000,  # длительность работы сенсора
-        poke_interval=60,  # частота проверки
-    )
-
-    drop_stg_table_before = SQLExecuteQueryOperator(
-        task_id="drop_stg_table_before",
-        conn_id=PG_CONNECT,
-        autocommit=True,
-        sql=f"""
-        DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
-        """,
-    )
-
-    create_stg_table = SQLExecuteQueryOperator(
-        task_id="create_stg_table",
-        conn_id=PG_CONNECT,
-        autocommit=True,
-        sql=f"""
-        CREATE TABLE stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}" AS
-        SELECT
-            time::date AS date,
-            count(*)
-        FROM
-            ods.fct_earthquake
-        WHERE
-            time::date = '{{{{ data_interval_start.format('YYYY-MM-DD') }}}}'
-        GROUP BY 1
-        """,
-    )
-
-    drop_from_target_table = SQLExecuteQueryOperator(
-        task_id="drop_from_target_table",
-        conn_id=PG_CONNECT,
-        autocommit=True,
-        sql=f"""
-        DELETE FROM {SCHEMA}.{TARGET_TABLE}
-        WHERE date IN
-        (
-            SELECT date FROM stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
-        )
-        """,
-    )
-
-    insert_into_target_table = SQLExecuteQueryOperator(
-        task_id="insert_into_target_table",
-        conn_id=PG_CONNECT,
-        autocommit=True,
-        sql=f"""
-        INSERT INTO {SCHEMA}.{TARGET_TABLE}
-        SELECT * FROM stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
-        """,
-    )
-
-    drop_stg_table_after = SQLExecuteQueryOperator(
-        task_id="drop_stg_table_after",
-        conn_id=PG_CONNECT,
-        autocommit=True,
-        sql=f"""
-        DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
-        """,
-    )
-
-    end = EmptyOperator(
-        task_id="end",
-    )
+    drop_stg_table_before_sql    = f"""DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+                                    """
+    create_stg_table_sql         = f"""CREATE TABLE         stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}" AS
+                                       SELECT time::date AS date,
+                                              count(*)
+                                       FROM   ods.fct_earthquake
+                                       WHERE  time::date = '{{{{ data_interval_start.format('YYYY-MM-DD') }}}}'
+                                       GROUP BY 1
+                                    """
+    drop_from_target_table_sql   = f"""DELETE FROM {SCHEMA}.{TARGET_TABLE}
+                                       WHERE date IN
+                                       (
+                                           SELECT date FROM stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+                                       )
+                                    """
+    insert_into_target_table_sql = f"""INSERT INTO {SCHEMA}.{TARGET_TABLE}
+                                       SELECT * FROM stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+                                    """
+    drop_stg_table_after_sql     = f"""DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+                                    """
 
     (
-            start >>
-            sensor_on_raw_layer >>
-            drop_stg_table_before >>
-            create_stg_table >>
-            drop_from_target_table >>
-            insert_into_target_table >>
-            drop_stg_table_after >>
-            end
+        EmptyOperator          (task_id = "start")                                                                                                 >>
+        ExternalTaskSensor     (task_id = "sensor_on_raw_layer", external_dag_id = "raw_from_s3_to_pg", 
+                                                                 allowed_states  = ["success"], 
+                                                                 mode            = "reschedule", 
+                                                                 timeout         = 360000, # длительность работы сенсора
+                                                                 poke_interval   = 60,     # частота проверки
+                                )                                                                                                                  >>
+        SQLExecuteQueryOperator(task_id = "drop_stg_table_before",    conn_id = PG_CONNECT, autocommit = True, sql = drop_stg_table_before_sql)    >>
+        SQLExecuteQueryOperator(task_id = "create_stg_table",         conn_id = PG_CONNECT, autocommit = True, sql = create_stg_table_sql)         >>
+        SQLExecuteQueryOperator(task_id = "drop_from_target_table",   conn_id = PG_CONNECT, autocommit = True, sql = drop_from_target_table_sql)   >>
+        SQLExecuteQueryOperator(task_id = "insert_into_target_table", conn_id = PG_CONNECT, autocommit = True, sql = insert_into_target_table_sql) >>
+        SQLExecuteQueryOperator(task_id = "drop_stg_table_after",     conn_id = PG_CONNECT, autocommit = True, sql = drop_stg_table_after_sql)     >>
+        EmptyOperator          (task_id = "end")
     )
--- dags/raw_from_api_to_s3.py
+++ dags/raw_from_api_to_s3.py
@@ -1,64 +1,68 @@
 import logging
-
 import duckdb
-import pendulum
-from airflow import DAG
-from airflow.models import Variable
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
+import pendulum                                     #для работы с датами
+from airflow                  import DAG
+from airflow.models           import Variable
+from airflow.operators.empty  import EmptyOperator  #оператор заглушка
+from airflow.operators.python import PythonOperator #исполнитель python кода
 
-# Конфигурация DAG
-OWNER = "i.korsakov"
-DAG_ID = "raw_from_api_to_s3"
+# переменные из метаданных AIRFLOW
+if True:
+    # Конфигурация DAG
+    OWNER             = "V.Gorenkov"
+    DAG_ID            = "raw_from_api_to_s3"
+    # Используемые таблицы в DAG
+    LAYER             = "raw"
+    SOURCE            = "earthquake"
+    # S3
+    ACCESS_KEY        = Variable.get("access_key")
+    SECRET_KEY        = Variable.get("secret_key")
+    LONG_DESCRIPTION  = """
+    # LONG DESCRIPTION
+    """
+    SHORT_DESCRIPTION = "SHORT DESCRIPTION"
+    args              = {
+                            "owner"      : OWNER,
+                            "start_date" : pendulum.datetime(2025 , 5  , 1, tz = "Europe/Moscow"),
+                            #retries (попытки повторного выполнения) — это встроенный механизм, позволяющий автоматически повторять выполнение упавшей задачи (таска) заданное количество раз с определенной задержкой перед тем, как отметить ее как окончательно провалившуюся, что обеспечивает отказоустойчивость и надежность ваших конвейеров данных (DAGs).
+                            "retries"    : 3,
+                            # Если catchup=True, Airflow выполнит все пропущенные интервалы, а при catchup=False пропустит их, начиная выполнение только с текущей даты.
+                            "catchup"    : True,
+                            "retry_delay": pendulum.duration(hours = 1),
+                        }
+# Другие переменные
+if True:
+    minio_bucket = 'main'
 
-# Используемые таблицы в DAG
-LAYER = "raw"
-SOURCE = "earthquake"
-
-# S3
-ACCESS_KEY = Variable.get("access_key")
-SECRET_KEY = Variable.get("secret_key")
-
-LONG_DESCRIPTION = """
-# LONG DESCRIPTION
-"""
-
-SHORT_DESCRIPTION = "SHORT DESCRIPTION"
-
-args = {
-    "owner": OWNER,
-    "start_date": pendulum.datetime(2025, 5, 1, tz="Europe/Moscow"),
-    "catchup": True,
-    "retries": 3,
-    "retry_delay": pendulum.duration(hours=1),
-}
-
-
+'''
+В Apache Airflow контекст (context) — это словарь (dictionary)
+с метаданными и переменными выполнения, который становится доступен внутри задач (tasks) и DAG во время их работы, 
+позволяя получать информацию о текущем запуске (dag_run), логической дате (logical_date), статусе задачи (task_instance) и 
+другой среде, что важно для шаблонизации с помощью Jinja, передачи параметров и взаимодействия через XCom.
+'''
 def get_dates(**context) -> tuple[str, str]:
     """"""
     start_date = context["data_interval_start"].format("YYYY-MM-DD")
-    end_date = context["data_interval_end"].format("YYYY-MM-DD")
-
+    end_date   = context["data_interval_end"]  .format("YYYY-MM-DD")
     return start_date, end_date
 
 
 def get_and_transfer_api_data_to_s3(**context):
     """"""
-
     start_date, end_date = get_dates(**context)
     logging.info(f"💻 Start load for dates: {start_date}/{end_date}")
     con = duckdb.connect()
 
     con.sql(
         f"""
-        SET TIMEZONE='UTC';
         INSTALL httpfs;
-        LOAD httpfs;
-        SET s3_url_style = 'path';
-        SET s3_endpoint = 'minio:9000';
-        SET s3_access_key_id = '{ACCESS_KEY}';
+        LOAD    httpfs;
+        SET TIMEZONE             = 'UTC';
+        SET s3_url_style         = 'path';
+        SET s3_endpoint          = 'minio:9000';
+        SET s3_access_key_id     = '{ACCESS_KEY}';
         SET s3_secret_access_key = '{SECRET_KEY}';
-        SET s3_use_ssl = FALSE;
+        SET s3_use_ssl           = FALSE;
 
         COPY
         (
@@ -66,38 +70,36 @@ def get_and_transfer_api_data_to_s3(**context):
                 *
             FROM
                 read_csv_auto('https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime={start_date}&endtime={end_date}') AS res
-        ) TO 's3://prod/{LAYER}/{SOURCE}/{start_date}/{start_date}_00-00-00.gz.parquet';
-
+        ) TO 's3://{minio_bucket}/{LAYER}/{SOURCE}/{start_date}/{start_date}_00-00-00.gz.parquet';
         """,
     )
 
     con.close()
     logging.info(f"✅ Download for date success: {start_date}")
 
-
+# Инициализация DAG'а
 with DAG(
-    dag_id=DAG_ID,
-    schedule_interval="0 5 * * *",
-    default_args=args,
-    tags=["s3", "raw"],
-    description=SHORT_DESCRIPTION,
-    concurrency=1,
-    max_active_tasks=1,
-    max_active_runs=1,
+    dag_id            = DAG_ID,
+                                          #| | | | |
+                                          #| | | | +----- День недели (0-7, где 0/7 - воскресенье)
+                                          #| | | +------- Месяц (1-12)
+                                          #| | +--------- День месяца (1-31)
+                                          #| +----------- Часы (0-23)
+                                          #+------------- Минуты (0-59)
+                                          #* - каждый
+    schedule_interval = "0 5 * * *",      # каждый день в 5 утра
+    default_args      = args,
+    tags              = ["s3", "raw"],
+    description       = SHORT_DESCRIPTION,
+    concurrency       = 1,                # исключение параллелизма
+    max_active_tasks  = 1,                # исключение параллелизма
+    max_active_runs   = 1,                # исключение параллелизма
 ) as dag:
     dag.doc_md = LONG_DESCRIPTION
 
-    start = EmptyOperator(
-        task_id="start",
+    (
+        EmptyOperator (task_id         = "start")                           >> 
+        PythonOperator(task_id         = "get_and_transfer_api_data_to_s3",
+                       python_callable =  get_and_transfer_api_data_to_s3)  >> 
+        EmptyOperator (task_id         = "end")
     )
-
-    get_and_transfer_api_data_to_s3 = PythonOperator(
-        task_id="get_and_transfer_api_data_to_s3",
-        python_callable=get_and_transfer_api_data_to_s3,
-    )
-
-    end = EmptyOperator(
-        task_id="end",
-    )
-
-    start >> get_and_transfer_api_data_to_s3 >> end
